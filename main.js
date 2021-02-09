f7321820726ec86f2122c1e99f1a242be1d53653
Islam Mohamed <islammohamedd1@gmail.com>
2021-02-09 21:07:35 +0300

    added more functionalities

--- main.js
+++ main.js
@@ -1,192 +1,254 @@
-import { KEYS } from './globals.js'
-import { Text } from './text.js'
-console.log('main.js')
+import { KEYS } from './globals.js';
+import { Text } from './text.js';
 
-const converter = new showdown.Converter()
+const converter = new showdown.Converter();
 
 const app = new Vue({
-	el: '#app',
+  el: '#app',
 
-	data: {
-		code: "السلام",
-		capsLock: null,
-	},
+  data: {
+    code: 'السلام',
+    capsLock: null,
+  },
 
-	created() {
-	},
+  created() {},
 
-	methods: {
-		insertLink() {
-			let link = window.prompt("Enter Link")
-			if (link) {
-				this.wrapSelected(['[', ']('+link+')'])
-			}
-		},
-		insertParenthesis() {
-			this.wrapSelected(['(', ')'])
-		},
-		insertBrackets() {
-			this.wrapSelected(['[', ']'])
-		},
-		insertCurlyBrackets() {
-			this.wrapSelected(['{', '}'])
-		},
-		insertImage() {
-			let link = window.prompt("Enter Image Link")
-			if (link) {
-				this.wrapSelected(['[', ']('+link+')'])
-			}
-		},
-		insertCode() {
-			this.wrapSelected(['```\n', '\n```'])
-		},
-		insertBold() {
-			this.wrapSelected(['**', '**'])
-		},
-		insertItalic() {
-			this.wrapSelected(['_', '_'])
-		},
-		wrapSelected([A, B]) {
-			let [start, end] = [$code.selectionStart, $code.selectionEnd];
-			let selectedText = this.code.substring(start, end);
-			let text = `${A}${selectedText}${B}`
-			document.execCommand('insertText', false, text)
-			let offset = A.length
-			$code.selectionStart = start + offset;
-			$code.selectionEnd = end + offset;
-			console.log("wrapSelected", text);
-		},
+  methods: {
+    insertLink() {
+      let link = window.prompt('Enter Link');
+      if (link) {
+        if (currentEditor === $preview) {
+          let selectedText = document.getSelection().toString();
+          console.log(selectedText.toString());
+          if (!selectedText || selectedText.length === 0) {
+            selectedText = link;
+          }
+          document.execCommand(
+            'insertHTML',
+            false,
+            '<a href="' + link + '" target="_blank">' + selectedText + '</a>'
+          );
+        } else {
+          this.wrapSelected(['[', '](' + link + ')']);
+        }
+      }
+    },
+    insertParenthesis() {
+      this.wrapSelected(['(', ')']);
+    },
+    insertBrackets() {
+      this.wrapSelected(['[', ']']);
+    },
+    insertCurlyBrackets() {
+      this.wrapSelected(['{', '}']);
+    },
+    insertImage() {
+      let link = window.prompt('Enter Image Link');
+      if (link) {
+        if (currentEditor === $preview) {
+          this.wrapSelected(['<a href="', '">' + link + '</a>']);
+        } else {
+          this.wrapSelected(['[', '](' + link + ')']);
+        }
+      }
+    },
+    insertCode() {
+      if (currentEditor === $preview) {
+        const selectedText = document.getSelection();
+        document.execCommand(
+          'insertHTML',
+          false,
+          `<pre><code>\n${selectedText}\n</code></pre><br>`
+        );
+      } else {
+        this.wrapSelected(['```\n', '\n```']);
+      }
+    },
+    insertBold() {
+      if (currentEditor === $preview) {
+        const [start, end] = [$preview.selectionStart, $preview.selectionEnd];
+        const selectedText = this.preview.substring(start, end);
+        document.execCommand('bold', false, selectedText);
+      } else {
+        this.wrapSelected(['**', '**']);
+      }
+    },
+    insertItalic() {
+      if (currentEditor === $preview) {
+        const selectedText = document.getSelection();
+        document.execCommand('italic', false, selectedText);
+      } else {
+        this.wrapSelected(['_', '_']);
+      }
+    },
+    wrapSelected([A, B]) {
+      let start, end, selectedText, text, offset;
+      console.log(currentEditor);
+      if (currentEditor === $code) {
+        [start, end] = [$code.selectionStart, $code.selectionEnd];
+        selectedText = this.code.substring(start, end);
+        text = `${A}${selectedText}${B}`;
+        document.execCommand('insertText', false, text);
+        offset = A.length;
+        $code.selectionStart = start + offset;
+        $code.selectionEnd = end + offset;
+      }
+      console.log('wrapSelected', text);
+    },
 
-		indent(evt) {
-			console.log('indent')
-			document.execCommand('insertText', false, '\t')
-		},
-		outdent(evt) {
-			console.log('outdent')
-			document.execCommand('outdent')
-		},
+    indent(evt) {
+      console.log('indent');
+      document.execCommand('insertText', false, '\t');
+    },
+    outdent(evt) {
+      console.log('outdent');
+      document.execCommand('outdent');
+    },
 
-		saveText() {
-			Text.Download(this.code, "saved.md")
-		}
-	},
+    saveText() {
+      Text.Download(this.code, 'saved.md');
+    },
+  },
 
-	computed: {
-		preview() {
-			return converter.makeHtml(this.code)
-		},
+  computed: {
+    preview() {
+      return converter.makeHtml(this.code);
+    },
 
-		lineNumbers() {
-			let numbers = [1];
-			for(let i = 0; i < this.code.length; i++) {
-				if (this.code[i] === '\n') {
-					numbers.push(numbers.length + 1)
-				}
-			}
-			return numbers.join("<br/>")
-		},
+    lineNumbers() {
+      let numbers = [1];
+      for (let i = 0; i < this.code.length; i++) {
+        if (this.code[i] === '\n') {
+          numbers.push(numbers.length + 1);
+        }
+      }
+      return numbers.join('<br/>');
+    },
 
-		length() {
-			return this.code.length;
-		},
-	}
-	
-})
-
+    length() {
+      return this.code.length;
+    },
+  },
+});
 
 // HTML Elements
-let $lineNumbers;
-let $code;
+let $lineNumbers,
+  $code,
+  $preview,
+  currentEditor,
+  $editorSelect,
+  $directionSelect;
+
 function initDocumentElements() {
-	$lineNumbers = document.getElementById('lineNumbers')
-	$code = document.getElementById('code');
+  $lineNumbers = document.getElementById('lineNumbers');
+  $code = document.getElementById('code');
+  $preview = document.getElementById('preview');
+  $directionSelect = document.getElementById('direction-select');
+  $editorSelect = document.getElementById('editor-select');
+
+  if ($directionSelect.value === 'ltr') {
+    changeDirection('ltr');
+  } else {
+    changeDirection('rtl');
+  }
+
+  $directionSelect.onchange = e => {
+    if (e.target.value === 'ltr') {
+      changeDirection('ltr');
+    } else {
+      changeDirection('rtl');
+    }
+  };
+
+  if ($editorSelect.value === 'markdown') {
+    showMarkdown();
+  } else {
+    hideMarkdown();
+  }
+  $editorSelect.onchange = e => {
+    if (e.target.value === 'markdown') {
+      showMarkdown();
+    } else {
+      hideMarkdown();
+    }
+  };
+  $code.addEventListener('focus', () => (currentEditor = $code));
+  $preview.addEventListener('focus', () => (currentEditor = $preview));
 }
 
-document.addEventListener('keydown', (evt) => {
-	// Update CapsLock state
-	app.capsLock = evt.getModifierState('CapsLock')
+const showMarkdown = () => {
+  currentEditor = $code;
+  $code.style.display = 'block';
+  $preview.style.gridColumn = '1';
+  $lineNumbers.style.display = 'block';
+};
 
-	// Ctrl + S: Saves (and downloads) markdown in ".md" format
-	if (evt.keyCode == KEYS.S && (evt.ctrlKey || evt.metaKey)) {
-		evt.preventDefault();
-		app.saveText()
-		return false;
-	}
-})
+const hideMarkdown = () => {
+  currentEditor = $preview;
+  $code.style.display = 'none';
+  $preview.style.gridColumn = 'span 2';
+  $lineNumbers.style.display = 'none';
+};
 
-function keypress(evt) {
-	const keyCode = evt.keyCode;
-	
-	switch(keyCode) {
+const changeDirection = direction => {
+  $code.style.direction = direction;
+  $preview.style.direction = direction;
+};
 
-		case KEYS.RIGHT_PAREN: {
-			evt.preventDefault();
-			app.insertParenthesis()
-			return false;
-		}
+document.addEventListener('keydown', evt => {
+  // Update CapsLock state
+  app.capsLock = evt.getModifierState('CapsLock');
 
-		case KEYS.RIGHT_BRACKET: {
-			evt.preventDefault();
-			app.insertBrackets()
-			return false;
-		}
-
-		case KEYS.RIGHT_CURLY: {
-			evt.preventDefault();
-			app.insertCurlyBrackets()
-			return false;
-		}
-	}
-	
-	// Print Arabic digits instead of English digits
-	// if (keyCode >= 0x30 && keyCode <= 0x39) {
-	// 	evt.preventDefault();
-	// 	const num = String.fromCodePoint((keyCode - 0x30) + 0x0660);
-	// 	console.log(num)
-	// 	document.execCommand('insertText', false, num)
-	// }
-}
-
+  // Ctrl + S: Saves (and downloads) markdown in ".md" format
+  if (evt.keyCode == KEYS.S && (evt.ctrlKey || evt.metaKey)) {
+    evt.preventDefault();
+    app.saveText();
+    return false;
+  }
+});
 
 // Drag and Drop loads text file
 // Prevent browser from viewing the file as a page upon dropping it accidentally outside the dropbox borders
-window.URL = window.URL || window.webkitURL
-window.addEventListener("dragenter", (e) => { e.preventDefault() });
-window.addEventListener("dragover", (e) => { e.preventDefault() });
-window.addEventListener("drop", (e) => { e.preventDefault() });
+window.URL = window.URL || window.webkitURL;
+window.addEventListener('dragenter', e => {
+  e.preventDefault();
+});
+window.addEventListener('dragover', e => {
+  e.preventDefault();
+});
+window.addEventListener('drop', e => {
+  e.preventDefault();
+});
 // Dropping a text.md file loads it.
-document.addEventListener('drop', function(evt) {
-	evt.preventDefault();
-	evt.stopPropagation();
-	Text.LoadFile(evt.dataTransfer.files[0])
-	.then(text => {
-		app.code = text
-	})
-	.catch(console.error)
-}, false);
-
+document.addEventListener(
+  'drop',
+  function (evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    Text.LoadFile(evt.dataTransfer.files[0])
+      .then(text => {
+        app.code = text;
+      })
+      .catch(console.error);
+  },
+  false
+);
 
 window.addEventListener('load', () => {
-	initDocumentElements()	
-	// bind text area with num of lines column (y-scrolling)
-	$code.addEventListener('scroll', (evt) => {
-		$lineNumbers.scrollTop = $code.scrollTop;
-	})
-})
+  initDocumentElements();
+  // bind text area with num of lines column (y-scrolling)
+  $code.addEventListener('scroll', evt => {
+    $lineNumbers.scrollTop = $code.scrollTop;
+  });
+});
 
-
-function keyup(evt) {
-	// Dot Separation of characters when capsLock is on.
-	// let text = app.code
-	// let codePoint = evt.key.codePointAt(0);
-	// let prevCodePoint = text.codePointAt(text.length - 2);
-	// console.log(`${prevCodePoint}=${String.fromCodePoint(prevCodePoint)}`, `${codePoint}=${String.fromCodePoint(codePoint)}`)
-	// if (app.capsLock && isAlphaArabic(codePoint) && isAlphaArabic(prevCodePoint)) {
-	// 	app.code = text.substring(0, text.length - 1) + '.' + text.charAt(text.length - 1)
-	// }
-}
-
-function isAlphaArabic(codePoint) {
-	// From Aliph to Yaa' excluding "Tatweel" letter.
-	return (codePoint >= 0x620 && codePoint <= 0x64A && codePoint !== 0x640)
-}
+var turndownService = new TurndownService();
+var codeElement = document.getElementById('code');
+document.getElementById('preview').addEventListener(
+  'input',
+  e => {
+    var markdown = turndownService.turndown(e.target.innerHTML);
+    codeElement.value = markdown;
+  },
+  false
+);
